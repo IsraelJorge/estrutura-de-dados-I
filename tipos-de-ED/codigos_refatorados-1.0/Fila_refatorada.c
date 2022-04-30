f4c40e10bdb5995a3a8fe239b8e8e74659d4c4a5
Israel Jorge <israeljorge2000@gmail.com>
2022-04-30 11:40:54 -0300

    main

--- tipos-de-ED/codigos_refatorados-1.0/Fila_refatorada.c
+++ tipos-de-ED/codigos_refatorados-1.0/Fila_refatorada.c
@@ -4,75 +4,84 @@
 #define SIZE 10
 
 typedef struct { 
+    int inicio;
     int fim;
+    int nItens;
     int dados[SIZE];
 } TFila;
 
 TFila fila;
 
 void enqueue(TFila *f,int info, int tamanho){
-    if (f->fim == tamanho - 1){
-        printf("\nFila cheia\n\n");
-        return; 
-    }
-
-    f->dados[++(f->fim)] = info;
-}
-
-void dequeue(TFila *f){
-    int i;
     
-    if(f->fim == -1){
-        printf("\nFila Vazia\n");
+    if(f->nItens == tamanho){
+        printf("\nFila Cheia!!!\n");
         return;
-    
-    }else{
-    
-        for (i = 0; i < f->fim; i++){
-            f->dados[i] = f->dados[i + 1];
-        }
-        (f->fim)--;
-
-        printf("\nElemento Removido da Fila!!");
     }
+
+    f->fim = (f->fim + 1) % tamanho;
+    f->dados[f->fim] = info;
+    f->nItens++;
+
 }
 
-void listar(TFila *f){
+void dequeue(TFila *f, int tamanho){
     int i;
+    
+    if(f->nItens == 0){
+        printf("\nFila Vazia!!!\n");
+        return;   
+    }
+    
+    f->inicio = (f->inicio + 1) % tamanho;
+    f->nItens--;
+
+    printf("\nElemento Removido da Fila!!!");
+    
+}
+
+void listar(TFila *f, int tamanho){
+    int i, n;
+
+    i = f->inicio;
+
     printf("\n\nListando\n");
     printf("\nElementos da Fila: ");
     
-    for (i = 0; i <= f->fim; i++){
-            printf("[%d] --> %d  |",i, f->dados[i]);
+    for (n = 0; n < f->nItens; n++){
+        printf("[%d] --> %d  |",i, f->dados[i]);
+
+        i = (i + 1) % tamanho;
     }    
 }
 
-int buscar(TFila *f,int chave){
-    int i;
+int buscar(TFila *f,int chave, int tamanho){
+    int n;
+    int i = f->inicio;
     
-    for(i = 0 ; i <= f->fim; i++){
+    for(n = 0 ; n < f->nItens; n++){
         
         if(f->dados[i] == chave){
             return i; 
         }
+        i = (i + 1) % tamanho;
     }
     return -1;
 }
 
 int main(int argc, char** argv) {
 
-    int leitura,key,opc;
+    int leitura, key, opc;
+    fila.inicio = 0;
     fila.fim = -1;
-    
+    fila.nItens = 0;
 
-    do
-    {
+    do{
         printf("\n====== EXEMPLO DE UMA FILA ======\n\n");
         printf("\n1 - Inserir elemento\n2 - Remover elemento\n3 - Mostrar Fila\n4 - Buscar Elemento\n0 - Sair\n\nOpcao? ");
         scanf("%d", &opc);
 
-        switch (opc)
-        {
+        switch (opc){
 
         case 1:
             printf("\nInforme o valor: ");
@@ -82,18 +91,18 @@ int main(int argc, char** argv) {
             break;
 
         case 2:
-            dequeue(&fila);
+            dequeue(&fila,SIZE);
             break;
 
         case 3:
-            listar(&fila);
+            listar(&fila,SIZE);
             break;
 
         case 4:
             printf("\nInforme a chave da busca: ");
             scanf("%d", &key);
 
-            int posicao = buscar(&fila,key);
+            int posicao = buscar(&fila,key,SIZE);
             if (posicao == -1)
                 printf("Chave da busca não encontrada na pilha\n\n");
             else
